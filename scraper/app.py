0a1a3831b8d635cf21d5733f1f72b0c30803dee7
Gutierrez <diego.gutierrez@oracle.com>
2024-09-20 12:17:11 -0600

    Added initial db connection script

--- scraper/app.py
+++ scraper/app.py
@@ -1,2 +1,9 @@
 # Scraper OCI Docs --> Clean --> Format --> Store in Vector Database, shared with API (AI Service)
 
+from pymilvus import connections, db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+conn = connections.connect(host=os.getenv('VDB_HOST'), port=os.getenv('VDB_PORT'), db_name=os.getenv('VDB_NAME'), token=os.getenv('VDB_TOKEN'))
+print(db.list_database())
