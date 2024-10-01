ab3c1495d6416e7375de73bae57bf591ce86f788
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-10-01 13:50:21 -0600

    Changes to scraper

--- scraper/app.py
+++ scraper/app.py
@@ -3,7 +3,13 @@
 from pymilvus import connections, db
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 conn = connections.connect(host=os.getenv('VDB_HOST'), port=os.getenv('VDB_PORT'), db_name=os.getenv('VDB_NAME'), token=os.getenv('VDB_TOKEN'))
 print(db.list_database())
+
+
+# req = requests.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=' + os.getenv('GEMINI_API_KEY'), headers={'Content-Type': 'application/json'}, data='{"contents":[{"parts":[{"text":"Explain how AI works"}]}]}')
+
+# print(req.json())
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import re
+import os
+from dotenv import load_dotenv
 # PRICES - https://www.oracle.com/cloud/price-list/
 
 def parse_sub_link(new_link):
@@ -13,19 +15,27 @@ def parse_sub_link(new_link):
 req = requests.get('https://www.oracle.com/cloud/price-list/')
 # print(req.content)
 
-soup = BeautifulSoup(req.content)
-print(re.sub(r"\n{3,}|\s{2,}", "\n", soup.get_text(separator='\n', strip=True)))
+soup = BeautifulSoup(req.content, features='html.parser')
 
-for link in BeautifulSoup(req.content, 'html.parser', parse_only=SoupStrainer('a')):
-    if link.has_attr('href'):
-        if '#' in link['href'] and 'pricing' in link['href']:
-            print(link['href'])
+# text = re.sub(r"\n{3,}|\s{2,}", "\n", soup.get_text(separator='\n', strip=True))
+text = soup.get_text(separator='\n', strip=True)
+
+# promt = "Interpret the following information from tables into price tables for these cloud services, return it in a clean an readable JSON format: " + text
+# load_dotenv()
+# req = requests.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=' + os.getenv('GEMINI_API_KEY'), headers={'Content-Type': 'application/json'}, data='{"contents":[{"parts":[{"text":"' + promt + '"}]}]}')
+
+# print(req.json())
+
+# for link in BeautifulSoup(req.content, 'html.parser', parse_only=SoupStrainer('a')):
+#     if link.has_attr('href'):
+#         if '#' in link['href'] and 'pricing' in link['href']:
+#             print(link['href'])
             
-            if 'http' in link['href']:
-                parse_sub_link(link['href'])
-            else:
-                parse_sub_link('https://www.oracle.com/cloud/price-list' + link['href'])
+#             if 'http' in link['href']:
+#                 parse_sub_link(link['href'])
+#             else:
+#                 parse_sub_link('https://www.oracle.com/cloud/price-list' + link['href'])
             
     
-    # input('GO')
+#             input('GO')
 
